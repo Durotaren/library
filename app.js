cdcd5c4afa5e561dbb134e50eb4914d50b910b3a
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-09-13 23:57:34 +0300

    Change the constructor function to a class

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const bookHolder = document.querySelector('.book-holder');
 const dialog = document.querySelector('dialog');
 const form = document.querySelector('form');
 
-function Book(title, author, numOfPages, isRead = false, uniqueId) {
-  this.title = title;
-  this.author = author;
-  this.numOfPages = numOfPages;
-  this.isRead = isRead;
-  this.uniqueId = uniqueId;
+class Book {
+  constructor(title, author, numOfPages, isRead = false, uniqueId) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.isRead = isRead;
+    this.uniqueId = uniqueId;
+  }
 }
 
 function addBookToLibrary(title, author, numOfPages, isread) {
@@ -20,63 +22,69 @@ function addBookToLibrary(title, author, numOfPages, isread) {
 }
 
 function createBookCard(book) {
-  let holderDiv = document.createElement("div");
-  holderDiv.classList.add("book");
+  let holderDiv = document.createElement('div');
+  holderDiv.classList.add('book');
 
   let btnHolderDiv = document.createElement('div');
   btnHolderDiv.classList.add('btnHolder');
-  
-  let removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
-  removeBtn.classList.add("removeBtn");
+
+  let removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+  removeBtn.classList.add('removeBtn');
   removeBtn.dataset.id = book.uniqueId;
 
-  let isReadBtn = document.createElement("button");
-  isReadBtn.textContent = book.isRead ? "I haven't read it" : "I have read it";
-  isReadBtn.classList.add(book.isRead ? "notReadBtn" : "readBtn");
-  isReadBtn.addEventListener("click", () => {
+  let isReadBtn = document.createElement('button');
+  isReadBtn.textContent = book.isRead ? "I haven't read it" : 'I have read it';
+  isReadBtn.classList.add(book.isRead ? 'notReadBtn' : 'readBtn');
+  isReadBtn.addEventListener('click', () => {
     book.isRead = !book.isRead;
 
-    isReadBtn.textContent = book.isRead ? 'I haven\'t read it' : 'I have read it';
+    isReadBtn.textContent = book.isRead
+      ? "I haven't read it"
+      : 'I have read it';
     let statusDiv = document.querySelector('.read-status');
-    statusDiv.textContent = book.isRead ? 'You have read the book' : 'You haven\'t read the book';
+    statusDiv.textContent = book.isRead
+      ? 'You have read the book'
+      : "You haven't read the book";
 
     isReadBtn.classList.remove(book.isRead ? 'readBtn' : 'notReadBtn');
     isReadBtn.classList.add(book.isRead ? 'notReadBtn' : 'readBtn');
   });
-  
-  removeBtn.addEventListener("click", () => {
+
+  removeBtn.addEventListener('click', () => {
     if (removeBtn.dataset.id === book.uniqueId) {
       holderDiv.remove();
-      library = library.filter(bookObj => bookObj.uniqueId !== removeBtn.dataset.id)
+      library = library.filter(
+        (bookObj) => bookObj.uniqueId !== removeBtn.dataset.id
+      );
     }
     console.log(library);
   });
 
-  for(let key in book) {
+  for (let key in book) {
     if (key === 'uniqueId') continue;
     let div = document.createElement('div');
 
-    switch(key) {
-      case 'title': 
-        div.textContent = `Title: ${book[key]}`
+    switch (key) {
+      case 'title':
+        div.textContent = `Title: ${book[key]}`;
         holderDiv.append(div);
         break;
 
       case 'author':
-        div.textContent = `Author: ${book[key]}`
+        div.textContent = `Author: ${book[key]}`;
         holderDiv.append(div);
         break;
 
       case 'numOfPages':
-        div.textContent = `Number of Pages: ${book[key]}`
+        div.textContent = `Number of Pages: ${book[key]}`;
         holderDiv.append(div);
         break;
 
       case 'isRead':
         div.classList.add('read-status');
         div.textContent = book[key]
-          ? "You have read the book."
+          ? 'You have read the book.'
           : "You haven't read the book yet.";
         break;
 
@@ -85,20 +93,18 @@ function createBookCard(book) {
     }
     holderDiv.append(div);
   }
-  btnHolderDiv.append(isReadBtn);  
+  btnHolderDiv.append(isReadBtn);
   btnHolderDiv.append(removeBtn);
   holderDiv.append(btnHolderDiv);
   bookHolder.append(holderDiv);
-  // A log to check library, to be removed later
-  console.log(library);
 }
 
 bookBtn.addEventListener('click', () => dialog.showModal());
 
-form.addEventListener("submit", function handleForm(event) {
+form.addEventListener('submit', function handleForm(event) {
   event.preventDefault();
 
-  let title = document.getElementById("title").value;
+  let title = document.getElementById('title').value;
   let author = document.getElementById('author').value;
   let numOfPages = document.getElementById('number-of-pages').value;
   let isread = document.getElementById('read-or-not').checked;
